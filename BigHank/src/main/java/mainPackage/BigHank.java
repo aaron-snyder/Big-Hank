5f875a33a33fd0c2905b6c808171d1ff25a592b9
Aaron Mervos <103236901+aaron-snyder@users.noreply.github.com>
2023-09-13 08:54:58 -0700

    Add files via upload

---
+++ BigHank/src/main/java/mainPackage/BigHank.java
@@ -0,0 +1,28 @@
+package mainPackage;
+
+import commands.*;
+import java.lang.reflect.Member;
+import java.util.*;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.*;
+import javax.security.auth.login.LoginException;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
+import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
+
+public class BigHank extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException
+    {
+        JDABuilder bot = JDABuilder.createDefault("OTYxODQ5MzMyMzM0NzQzNTUy.GKuwJH.0QMF0RNxg3MzYX2NAxBnJaZeS5UYTEICfZaI6E");
+        bot.setActivity(Activity.playing("bantz-big-bass-fishing")); // Sets bot's Discord activity
+        bot.addEventListeners(new BotCommands()); // BotCommands class become event listener
+        bot.enableIntents(GatewayIntent.GUILD_MEMBERS); // Allows bot to read from users
+        bot.enableIntents(GatewayIntent.MESSAGE_CONTENT); // Allows bot to send messages
+        bot.build();
+    }
+}
